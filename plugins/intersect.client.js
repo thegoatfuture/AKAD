38053a87326e9392519f46039d9950c8b1dd10e6
thegoatfuture <mathis.fanget@icloud.com>
2025-06-04 23:29:26 +0200

    Add disconnect cleanup to intersect directive

--- plugins/intersect.client.js
+++ plugins/intersect.client.js
@@ -1,19 +1,26 @@
 // plugins/v-intersect.js
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.directive('intersect', {
-      mounted(el, binding) {
-        const options = {
-          root: null,
-          rootMargin: '0px',
-          threshold: 0.1,
-        }
-        const observer = new IntersectionObserver(([entry]) => {
-          if (entry.isIntersecting) {
-            binding.value?.callback?.()
-            if (binding.value?.once) observer.unobserve(entry.target)
-          }
-        }, options)
-        observer.observe(el)
+  nuxtApp.vueApp.directive('intersect', {
+    mounted(el, binding) {
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
       }
-    })
+
+      const observer = new IntersectionObserver(([entry]) => {
+        if (entry.isIntersecting) {
+          binding.value?.callback?.()
+          if (binding.value?.once) observer.unobserve(entry.target)
+        }
+      }, options)
+
+      observer.observe(el)
+      // Store observer on the element for cleanup
+      el._intersectionObserver = observer
+    },
+    beforeUnmount(el) {
+      el._intersectionObserver?.disconnect()
+    },
   })
+})
