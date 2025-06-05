0afe5e6f0ba2e8c945f6ad386863668dee390848
thegoatfuture <mathis.fanget@icloud.com>
2025-06-05 21:56:46 +0200

    Enhance cursor glow effect

--- components/CursorGlow.vue
+++ components/CursorGlow.vue
@@ -1,6 +1,7 @@
 <template>
   <div ref="cursor" class="cursor-glow fixed pointer-events-none z-50 transition-transform duration-100 ease-out">
-    <div class="w-8 h-8 bg-yellow-400/20 rounded-full blur-xl transform scale-150"></div>
+    <div class="w-16 h-16 bg-yellow-400/30 rounded-full blur-2xl transform scale-150"></div>
+    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-8 h-8 bg-yellow-400/20 rounded-full blur-xl"></div>
   </div>
 </template>
 
@@ -15,7 +16,8 @@ function updateCursorPosition(e) {
   const x = e.clientX
   const y = e.clientY
   
-  cursor.value.style.transform = `translate(${x - 16}px, ${y - 16}px)`
+  // Center the glow effect precisely under the cursor
+  cursor.value.style.transform = `translate(${x - 32}px, ${y - 32}px)`
 }
 
 onMounted(() => {
@@ -30,5 +32,6 @@ onBeforeUnmount(() => {
 <style scoped>
 .cursor-glow {
   will-change: transform;
+  pointer-events: none;
 }
 </style>
