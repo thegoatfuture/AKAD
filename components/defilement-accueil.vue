47eecf4e6af80cf110dfea72cc0204424d15a5bc
thegoatfuture <mathis.fanget@icloud.com>
2025-06-05 14:43:22 +0200

    Update TradingView Widget Implementation

--- components/defilement-accueil.vue
+++ components/defilement-accueil.vue
@@ -10,29 +10,64 @@
 import { onMounted } from 'vue'
 
 onMounted(() => {
-  if (window.TradingView) return
-
   const script = document.createElement('script')
-  script.src =
-    'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js'
+  script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js'
   script.async = true
+  script.type = 'text/javascript'
+  
+  // Configure the widget
   script.innerHTML = JSON.stringify({
     symbols: [
-      { proName: 'FOREXCOM:EURUSD', title: 'EUR/USD' },
-      { proName: 'FOREXCOM:GBPUSD', title: 'GBP/USD' },
-      { proName: 'FOREXCOM:USDJPY', title: 'USD/JPY' },
-      { proName: 'OANDA:XAUUSD', title: 'XAU/USD' },
-      { proName: 'BITSTAMP:BTCUSD', title: 'BTC/USD' },
+      {
+        proName: 'FOREXCOM:EURUSD',
+        title: 'EUR/USD'
+      },
+      {
+        proName: 'FOREXCOM:GBPUSD',
+        title: 'GBP/USD'
+      },
+      {
+        proName: 'FOREXCOM:USDJPY',
+        title: 'USD/JPY'
+      },
+      {
+        proName: 'BITSTAMP:BTCUSD',
+        title: 'BTC/USD'
+      },
+      {
+        proName: 'OANDA:XAUUSD',
+        title: 'Gold'
+      }
     ],
-    showSymbolLogo: true,
-    colorTheme: 'dark', // Affiche le texte en blanc
-    isTransparent: true, // Fond transparent
+    colorTheme: 'dark',
+    isTransparent: true,
     displayMode: 'adaptive',
     locale: 'fr',
+    showSymbolLogo: true,
+    width: '100%',
+    height: 46
   })
 
-  document
-    .querySelector('.tradingview-widget-container__widget')
-    ?.appendChild(script)
+  // Only append if widget container exists and script hasn't been added yet
+  const widgetContainer = document.querySelector('.tradingview-widget-container__widget')
+  if (widgetContainer && !document.querySelector('script[src*="embed-widget-ticker-tape.js"]')) {
+    widgetContainer.appendChild(script)
+  }
+
+  // Cleanup on component unmount
+  return () => {
+    const scriptElement = document.querySelector('script[src*="embed-widget-ticker-tape.js"]')
+    if (scriptElement) {
+      scriptElement.remove()
+    }
+  }
 })
 </script>
+
+<style scoped>
+.tradingview-widget-container {
+  width: 100%;
+  height: 46px;
+  overflow: hidden;
+}
+</style>
