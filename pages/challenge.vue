70210e94f000d74893dd096e716e750f88ef05e7
thegoatfuture <mathis.fanget@icloud.com>
2025-06-06 10:05:11 +0200

    Fix SVG stroke attribute syntax error

--- pages/challenge.vue
+++ pages/challenge.vue
@@ -152,7 +152,8 @@
                 <span>Maximum 10% de drawdown total</span>
               </li>
               <li class="flex items-center gap-2">
-                <svg class="w-4 h-4 text-yellow-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="width="2" d="M5 13l4 4L19 7"/>
+                <svg class="w-4 h-4 text-yellow-400 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"/>
                 </svg>
                 <span>Levier maximum de 1:100</span>
               </li>
