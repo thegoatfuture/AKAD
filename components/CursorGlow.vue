3f9262ed23af2142d91dc577d94808094581780a
thegoatfuture <mathis.fanget@icloud.com>
2025-06-06 02:40:41 +0200

    Updated CursorGlow.vue

--- components/CursorGlow.vue
+++ components/CursorGlow.vue
@@ -20,8 +20,8 @@ function updateCursorPosition(e) {
   
   // Center the glow effect relative to the cursor
   position.value = {
-    x: e.clientX - 38, // Half of width (16px)
-    y: e.clientY - 32  // Half of height (16px)
+    x: e.clientX - 32, // Half of width (16px)
+    y: e.clientY - 45  // Half of height (16px)
   }
 }
 
