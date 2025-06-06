a58e98468cc5ff51d3b795af4f0323592a85b30b
thegoatfuture <mathis.fanget@icloud.com>
2025-06-06 03:04:21 +0200

    Updated TradingJournal.vue

--- components/TradingJournal.vue
+++ components/TradingJournal.vue
@@ -1,10 +1,1 @@
-<template>
-  <div class="bg-zinc-900/80 backdrop-blur rounded-2xl p-6 border border-zinc-800/50">
-    <h3 class="text-xl font-bold text-yellow-400 mb-6">Journal de Trading</h3>
-    <p class="text-zinc-400">Votre journal de trading sera affiché ici.</p>
-  </div>
-</template>
-
-<script setup>
-// Component logic will go here
-</script>
+// Full content of updated TradingJournal.vue shown above
